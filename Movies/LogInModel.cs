c4377359df6def98b08c611059908651a4acf61a
sylvainrey <sylvain.rey.pro@gmail.com>
2019-06-21 15:07:55 +0200

    Adding elements of the main page and the login page with its associated model

---
+++ Movies/LogInModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Movies
+{
+    public class LogInModel
+    {
+        // constructeur
+        public LogInModel()
+        {
+
+        }
+    }
+}
---
+++ Movies/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace Movies
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            Name = "Veuillez vous identifier";
+            MyCommand = new RelayCommand(MyCommandExecute, MyCommandCanExecute);
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand MyCommand { get; } 
+
+        void MyCommandExecute()
+        {
+            new LogIn();
+        }
+
+        bool MyCommandCanExecute()
+        {
+            return Name!="Bienvenue";
+        }
+    }
+}
